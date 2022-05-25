38bb035e5b036e103c24a44db5546156a320bdd6
Adam Beck <adamb3189@gmail.com>
2022-05-25 16:59:56 +1000

    stash

--- src/pages/api/flags/index.ts
+++ src/pages/api/flags/index.ts
@@ -1,4 +1,5 @@
 import {Flag} from '@prisma/client'
+import {string, object, boolean, InferType, ValidationError} from 'yup'
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {
   Controller,
@@ -18,7 +19,18 @@ type NewFlagPostBody = {
   active: boolean
 }
 
+type NewFlagBody = InferType<typeof FlagController.newFlagSchema>
+
 export class FlagController extends Controller {
+  static newFlagSchema = object({
+    name: string()
+      .required('A new flag requires a name'),
+    active: boolean()
+      .required(
+        'A new flag needs an active state. It can be either true or false.',
+      ),
+  })
+
   async get(request: NextApiRequest, response: NextApiResponse) {
     const page = getPageFromQuery(request)
     const {name} = getQuery<FindByNameQuery>(request)
@@ -47,18 +59,77 @@ export class FlagController extends Controller {
   }
 
   async post(request: NextApiRequest, response: NextApiResponse) {
-    const {active, name} = request.body as NewFlagPostBody
-    const flag = await Repositorys.find(FlagRepository).create({
-      name,
-      active,
+    const value = await Try
+      .of(async () => FlagController
+        .newFlagSchema
+        .validate(request.body)
+      )
+
+    await value.left((error: any) => {
+      response.status(400).json({
+        errors: error.errors,
+      })
     })
 
-    response.json({
-      data: {
-        flag,
-      },
-    });
+    await value.right(async (body: Promise<NewFlagBody>) => {
+      const result = await Try<Promise<Flag>, Error>(async () => {
+        const { name, active } = await body
+
+        return Repositorys.find(FlagRepository).create({
+          name: name,
+          active: active,
+        }) as Promise<Flag>
+      })
+
+      result.right(async (flag: Promise<Flag>) => {
+        response.json({
+          data: {
+            flag: await flag,
+          },
+        })
+      })
+
+      result.left((error: Error) => {
+        response.status(500).json({
+          errors: [error.message]
+        })
+      })
+    })
   }
 }
 
 export default install(FlagController)
+
+class Try {
+  public error
+  public value
+
+  static async of(func: Function) {
+    return new Try(await func())
+  }
+
+  constructor(value?: Awaited<any>, error?: Error) {
+    this.value = value || null
+    this.error = error || null
+  }
+
+  static async left (func: () => Awaited<any>) {
+    return Try.of(() => func())
+  }
+
+  async left(func: (value: Error) => Awaited<any>) {
+    const awaitedError = await this.error
+
+    if(awaitedError) {
+      return Try.of(() => func(awaitedError))
+    }
+  }
+
+  async right(func: (value: Awaited<any>) => Awaited<any>) {
+    const awaitedValue = await this.value
+
+    if(awaitedValue) {
+      return Try.of(() => func(awaitedValue))
+    }
+  }
+}
