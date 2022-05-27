46ba3b5edd1a636b0f49fbee79c7dd2a4cb455e6
Adam Beck <adamb3189@gmail.com>
2022-05-27 15:00:12 +1000

    feat: support updating flags

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,7 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', 'src/'],
 }
 
+
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
--- tests/pages/api/flags/index.spec.ts
+++ tests/pages/api/flags/index.spec.ts
@@ -2,6 +2,7 @@ import {expect} from 'chai'
 
 import {
   get,
+  post,
   Repositorys,
   RequestBuilder,
   ResponseType,
@@ -9,6 +10,7 @@ import {
 import {Flag} from '@prisma/client'
 import {FlagRepository} from '../../../../src/repositories/flag-repository'
 import {FlagController} from '../../../../src/pages/api/flags'
+import { ValidationError } from 'yup'
 
 describe('FlagController', () => {
   let flag: Flag
@@ -49,7 +51,7 @@ describe('FlagController', () => {
         Repositorys
           .find(FlagRepository)
           .mock('findByName', jest.fn(() => [flag]))
-        response = await get(FlagController, request)
+        await get(FlagController, request)
       })
 
       afterEach(() => {
@@ -67,6 +69,91 @@ describe('FlagController', () => {
           .mock.calls[0] as Array<Record<string, string>>
 
         expect(mockCalls).to.deep.equal([{name: 'A name', page: 1}])
+      })
+    })
+  })
+
+  describe('#post', () => {
+    let request, response, flag
+
+    beforeEach(async () => {
+      flag = {
+        name: 'a name',
+        active: true
+      }
+      request = new RequestBuilder().body(flag)
+      Repositorys.find(FlagRepository).mock('create', jest.fn(() => flag))
+      response = await post(FlagController, request)
+    })
+
+    afterEach(() => {
+      Repositorys.find(FlagRepository).reset('create')
+    })
+
+    it('creates a new flag', () => {
+      const mock = (Repositorys.find(FlagRepository).create) as jest.Mock
+      expect(mock.mock.calls[0][0]).to.deep.equal({
+        active: true,
+        name: 'a name'
+      })
+    })
+
+    it('sends the flag back in a response', () => {
+      expect(response.json).to.deep.equal({
+        data: {
+          flag
+        }
+      })
+    })
+
+    describe('when the post request has incorrect parameters', () => {
+      beforeEach(async () => {
+        flag = {
+          active: 'true'
+        }
+        request = new RequestBuilder().body(flag)
+        Repositorys.find(FlagRepository).mock('create', jest.fn(() => {
+          throw new ValidationError('')
+        }))
+        response = await post(FlagController, request)
+      })
+
+      afterEach(() => {
+        Repositorys.find(FlagRepository).reset('create')
+      })
+
+      it('returns a 400', () => {
+        expect(response.status).to.equal(400)
+      })
+
+      it('includes the error in the response', () => {
+        expect(response.json).to.deep.equal({
+          errors: ['A new flag requires a name']
+        })
+      })
+    })
+
+    describe('when the database can\'t be reached', () => {
+      beforeEach(async () => {
+        flag = {
+          active: 'true',
+          name: 'A name'
+        }
+        request = new RequestBuilder().body(flag)
+        Repositorys.find(FlagRepository).mock('create', jest.fn(() => {
+          throw new Error('I can\'t talk to the database')
+        }))
+        response = await post(FlagController, request)
+      })
+
+      it('returns a 500', () => {
+        expect(response.status).to.equal(500)
+      })
+
+      it('includes the error in the response', () => {
+        expect(response.json).to.deep.equal({
+          errors: ['I can\'t talk to the database']
+        })
       })
     })
   })
